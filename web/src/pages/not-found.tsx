de47b930f934c57110d594b0c2e0a3059f0ba036
jalun118 <jalunugroho118@gmail.com>
2024-12-19 21:08:04 +0700

    feat(not-found-page): create page handle route not found

---
+++ web/src/pages/not-found.tsx
@@ -0,0 +1,13 @@
+import NotFoundIcon from "../components/NotFoundIcon";
+
+export default function NotFoundPage() {
+  return (
+    <div className="container">
+      <div className="m-auto max-w-md">
+        <div>
+          <NotFoundIcon />
+        </div>
+      </div>
+    </div>
+  );
+}
