76405ea598059db193859689c3f5d34d6f9152aa
jalun118 <jalunugroho118@gmail.com>
2024-12-19 21:07:27 +0700

    feat(icon-not-found): create icon for not found data or not found page

---
+++ web/src/components/NotFoundIcon.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundIcon() {
+  return (
+    <div className="flex flex-col items-center justify-center py-20">
+      <div className="text-4xl font-bold">{"ㄟ( ▔, ▔ )ㄏ"}</div>
+      <div className="mt-4 text-2xl font-bold">Not Found</div>
+    </div>
+  );
+}
