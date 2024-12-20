b2b8846cc30f218af41934f371ab5c01fd1267cb
jalun118 <jalunugroho118@gmail.com>
2024-12-20 20:21:49 +0700

    feat(verify-forgot-password-page): create page setting forgot password

---
+++ web/src/pages/verify/page.tsx
@@ -0,0 +1,210 @@
+import { useFormik } from "formik";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { object as yupObject, string as yupString } from "yup";
+
+interface iDefaultValue {
+  new_password: string;
+  confirm_password: string;
+}
+
+const initValue: iDefaultValue = {
+  confirm_password: "",
+  new_password: "",
+};
+
+const verifySchema = yupObject({
+  confirm_password: yupString()
+    .required("Required")
+    .min(4, "minimum password 4 character"),
+  new_password: yupString()
+    .required("Required")
+    .min(4, "minimum password 4 character"),
+});
+
+export default function VerifyForgotPasswordPage() {
+  const { id } = useParams();
+
+  const [isShowFields, setHideFields] = useState({
+    new_password: false,
+    confirm_password: false,
+  });
+
+  function HandleToggleShow(field: keyof iDefaultValue) {
+    setHideFields((prev) => ({
+      ...prev,
+      [field]: !prev[field],
+    }));
+  }
+
+  const { errors, touched, values, handleChange, handleSubmit } = useFormik({
+    initialValues: initValue,
+    onSubmit: (value, action) => {
+      if (value.confirm_password !== value.new_password) {
+        action.setErrors({ confirm_password: "Password not match" });
+        return;
+      }
+
+      console.log({ value, id });
+
+      location.replace("/");
+    },
+    validationSchema: verifySchema,
+  });
+
+  return (
+    <div className="container">
+      <div className="m-auto max-w-md">
+        <h1 className="text-2xl font-semibold">Verify</h1>
+        <div className="mt-4">
+          <form onSubmit={handleSubmit}>
+            <div className="mt-2">
+              <label
+                htmlFor="new_password"
+                className="mb-2 block font-semibold"
+              >
+                New Password
+              </label>
+              <div className="relative">
+                <input
+                  type={isShowFields.new_password ? "text" : "password"}
+                  id="new_password"
+                  name="new_password"
+                  autoComplete="off"
+                  autoCorrect="off"
+                  aria-autocomplete="none"
+                  spellCheck="false"
+                  className="block w-full rounded-md px-3 py-2 text-black"
+                  value={values.new_password}
+                  onChange={handleChange}
+                />
+                <button
+                  onClick={() => HandleToggleShow("new_password")}
+                  className="absolute inset-y-0 right-2"
+                >
+                  {isShowFields.new_password ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+              {touched.new_password && errors.new_password && (
+                <div className="text-sm font-semibold text-red-500">
+                  {errors.new_password}
+                </div>
+              )}
+            </div>
+            <div className="mt-2">
+              <label
+                htmlFor="confirm_password"
+                className="mb-2 block font-semibold"
+              >
+                Confirm Password
+              </label>
+              <div className="relative">
+                <input
+                  type={isShowFields.confirm_password ? "text" : "password"}
+                  id="confirm_password"
+                  name="confirm_password"
+                  autoComplete="off"
+                  autoCorrect="off"
+                  aria-autocomplete="none"
+                  spellCheck="false"
+                  className="block w-full rounded-md px-3 py-2 text-black"
+                  value={values.confirm_password}
+                  onChange={handleChange}
+                />
+                <button
+                  onClick={() => HandleToggleShow("confirm_password")}
+                  className="absolute inset-y-0 right-2"
+                >
+                  {isShowFields.confirm_password ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
+              {touched.confirm_password && errors.confirm_password && (
+                <div className="text-sm font-semibold text-red-500">
+                  {errors.confirm_password}
+                </div>
+              )}
+            </div>
+            <div className="mt-5">
+              <button
+                type="submit"
+                className="rounded-md bg-blue-600 px-4 py-2 font-semibold text-white active:bg-blue-700"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
